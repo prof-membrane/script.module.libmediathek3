e90e91c737bdb5a121e45952909dfe4d738b3288
Max <max.christoph@web.de>
2019-03-13 21:32:53 +0100

    improved time dialog function

--- lib/libmediathek3dialogs.py
+++ lib/libmediathek3dialogs.py
@@ -9,14 +9,19 @@ import xbmcgui
 import xbmcaddon
 import xbmcvfs
 import re
-from datetime import datetime,timedelta
+from datetime import datetime
 import time
 
 import libmediathek3utils
 
-def dialogDate():
+def dialogDate(format='%d%m%Y'):
 	dialog = xbmcgui.Dialog()
-	return dialog.numeric(1, libmediathek3utils.getTranslation(31030)).replace('/','').replace(' ','0')
+	result = dialog.numeric(1, libmediathek3utils.getTranslation(31030)).replace("/", "").replace(" ", "0")
+	try:
+		result = datetime.strptime(result, '%d%m%Y')
+	except TypeError:  # handle bug in Python 2
+		result = datetime(*(time.strptime(result, '%d%m%Y')[0:6]))
+	return result.strftime(format)
 	
 def getSearchString():
 	dialog = xbmcgui.Dialog()
